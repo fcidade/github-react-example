2ee91ec39215a6ced5d0526b8976233c206adcfc
Francisco <juniorfchj2@gmail.com>
2019-09-02 10:46:05 -0300

    Added JSDoc

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import UserDetails from './pages/UserDetails';
 
+/**
+ * Represents the application container.
+ * @returns {object} Returns all the JSX for the entire application.
+ */
 function App() {
+  console.log(typeof <BrowserRouter/>);
   return (
     <BrowserRouter>
 			<Route path='/' exact component={Home}/>
--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/**
+ * Draw our application on a HTML div tag.
+ * @param {object} app - Application component.
+ * @param {Element} element - Element where the application will be placed.
+ */
 ReactDOM.render(<App />, document.getElementById('root'));
